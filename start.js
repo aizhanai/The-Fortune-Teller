0642a3e265198f5017cfdab7f73d0f6f71916b3a
Aizhanai <aijanam91@gmail.com>
2021-03-02 15:49:36 +0100

    first commit with JS

---
+++ start.js
@@ -0,0 +1,6 @@
+var child = (4);
+var partner = ("Phillip");
+var  loc = ("New York");
+var job = (IT Manager);
+
+console.log("You will be a " +job+ " in " + loc+ ", " +" and get married to "+partner + "with " + child +"kids" +".");
